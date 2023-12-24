b6f289e81aa6fbf1cab87cb3ba3c0ddae94472a2
piepie <a84752321596@gmail.com>
2023-12-25 00:54:57 +0800

    canvas game

--- neutec-recruit-exam/src/router.js
+++ neutec-recruit-exam/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Demo from '@/views/demo.vue'
 import AdvancedDemo from '@/views/advanced_demo.vue'
+import CanvasDemo from '@/views/canvas_demo.vue'
 
 export const router = createRouter({
     history: createWebHistory(),
@@ -12,6 +13,10 @@ export const router = createRouter({
         {
             path: '/advanced_demo',
             component: AdvancedDemo
+        },
+        {
+            path: '/canvas_demo',
+            component: CanvasDemo
         },
     ]
 })
